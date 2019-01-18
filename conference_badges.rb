2afbc1108e714dbf12540547178ec074312082ed
Bryn Bennett <bryn.bennett@gmail.com>
2019-01-18 23:13:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(guest_list)
+  badge_list = []
+    guest_list.each {|guest_name| badge_list << "Hello, my name is #{guest_name}."}
+  return badge_list
+end
+
+def assign_rooms(guest_list)
+  room_assign = []
+  guest_list.each_with_index {|name, index| room_assign << "Hello, #{name}! You'll be assigned to room #{index+1}!"}
+  return room_assign
+end
+
+def printer(guest_list)
+  badge_with_room = []
+  guest_list.each {badge_with_room << 
+end
